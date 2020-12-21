aacf21146885349ca969f0ddc4a1cc8e5f5cf475
laithsobuh <38588487+laithsobuh@users.noreply.github.com>
2020-12-21 20:50:44 +0200

    Initial commit

--- app/src/main/java/com/example/ahmadprojectui/MainActivity.java
+++ app/src/main/java/com/example/ahmadprojectui/MainActivity.java
@@ -15,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     login=findViewById(R.id.button10);
     signup=findViewById(R.id.button11);
+    guest=findViewById(R.id.button12);
+    guest.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i=new Intent(MainActivity.this,Home.class);
+            startActivity(i);
+        }
+    });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
