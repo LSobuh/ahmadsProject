53c95af4b9e9f91a5bfdad84284f2ac152679c86
laithsobuh <38588487+laithsobuh@users.noreply.github.com>
2020-12-21 16:24:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmadprojectui/Login.java
@@ -0,0 +1,19 @@
+package com.example.ahmadprojectui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+
+    }
+
+}
