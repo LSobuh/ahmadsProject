71e331685fc7955fe27a9aab2d82f99d0da31dda
laithsobuh <38588487+laithsobuh@users.noreply.github.com>
2020-12-26 21:54:36 +0200

    Initial commit

--- app/src/main/java/com/example/ahmadprojectui/Login.java
+++ app/src/main/java/com/example/ahmadprojectui/Login.java
@@ -2,17 +2,26 @@ package com.example.ahmadprojectui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-
+login=findViewById(R.id.button);
+login.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent i=new Intent(Login.this,Home.class);
+        startActivity(i);
+    }
+});
 
     }
 
